e67a283c1a1f12c456ffa201c08dfc7d3d38003e
SEMANA 9 <aj.villaltaf@uea.edu.ec>
2026-09-20 18:03:58 -0500

    matriz = [     [2, 4, 6],     [1, 3, 5],     [7, 8, 9] ]
    for i in range(3):
        for j in range(3):
            print(matriz[i][j], end=" ")
        print()

---
+++ SEMANA 13/FuncionCompra.py
@@ -0,0 +1,13 @@
+    o)
+    print("El total de la compra es:", resultad
+
+    resultado = calcular_total(precio, cantidad)
+
+    cantidad = 3
+    precio = 10
+if __name__ == "__main__":
+
+
+    return total
+    total = precio * cantidad
+def calcular_total(precio, cantidad):
